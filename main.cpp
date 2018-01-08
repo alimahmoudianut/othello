06bcf389fbfb49844e7a3158cbab1568314fecc1
Ali <ali.mahmoudian@ut.ac.ir>
2018-01-08 23:34:41 +0330

    Configure QT project and start working on logic part.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    qDebug() << "We have launched our project.";
+    return a.exec();
+}
